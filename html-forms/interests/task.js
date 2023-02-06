8786336a777ac0a03f3a49b684973d85016f629b
AleksMax <0.5ctakah@gmail.com>
2023-02-06 13:48:52 +0300

    add html-form

--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -1,6 +1,19 @@
 const interests = document.querySelectorAll('.interest');
-const interestsUl = document.querySelectorAll('.interests_active');
-const checkBox = document.querySelectorAll('.interest__check');
-for (let i = 0;i < interests.length;i++){
 
-}
+for (let i = 0;i < interests.length;i++){
+    if (interests[i].querySelector('.interests_active')){
+        let checkBoxChild = interests[i].querySelector('.interests_active').querySelectorAll('.interest__check')
+        interests[i].querySelector('.interest__check').addEventListener('change', () => {
+            if (interests[i].querySelector('.interest__check').checked){
+                 for (let i=0;i < checkBoxChild.length;i++){
+                    checkBoxChild[i].checked = true;
+                 }
+                 } else {
+                    for (let i=0;i < checkBoxChild.length;i++){
+                      checkBoxChild[i].checked = false;
+                     }
+                 }
+            }
+        )
+        }    
+    }
