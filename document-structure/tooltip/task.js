a005cb91fa832fd22100c395db1800e59b41e12e
AleksMax <0.5ctakah@gmail.com>
2023-02-17 12:28:58 +0300

    doc-struct fix

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,4 +1,4 @@
-//tooltip_active
+/*tooltip_active
 const tip = document.querySelectorAll('.has-tooltip')
 let newEl = document.createElement('div');
 newEl.classList.add('tooltip', 'tooltip_active')
@@ -10,4 +10,27 @@ for (let i=0;i<tip.length;i++){
         tip[i].appendChild(newEl)
         return false;
     })
-}
+} */
+
+const hasTooltip = document.querySelectorAll('.has-tooltip');
+
+hasTooltip.forEach((item) => {
+  item.addEventListener('click', (event) => {
+    event.preventDefault();
+    const tooltipActive = document.querySelectorAll('.tooltip_active')[0];
+    if (tooltipActive) {
+      tooltipActive.classList.remove('tooltip_active');
+        if (tooltipActive.textContent == item.title) {
+        return false;
+        }
+    }
+  const tooltip = document.createElement('div');
+  tooltip.classList.add('tooltip', 'tooltip_active');
+  tooltip.textContent = item.getAttribute('title');
+
+  const coordinates = item.getBoundingClientRect();
+  tooltip.style.top = coordinates.bottom + 3 + 'px';
+  tooltip.style.left = coordinates.left + 'px';
+  document.body.append(tooltip); 
+  })
+})
